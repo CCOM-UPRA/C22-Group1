e5a24e22d0ac9f3020d0ab1a0628cc551a273e62
Jean029 <jean.serrano7@upr.edu>
2023-06-09 15:09:47 -0500

    conexion a la base de datos

--- Ecommerce/web/__init__.py
+++ Ecommerce/web/__init__.py
@@ -1,16 +1,6 @@
 from flask import Flask, session
 import mysql.connector
 
-database = mysql.connector.connect(
-    host='sql9.freemysqlhosting.net',
-    port=3306,
-    user='sql9607914',
-    password='uWzaFxXgrH',
-    database='sql9607914'
-)
-
-session['cursor'] = database.cursor()
-
 
 def create_app():
     app = Flask(__name__)
