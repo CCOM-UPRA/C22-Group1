4d47c1cb5625bb918356d7065be9a0327cf5ec3c
Yadiel Cruzado <yadiel.cruzado@upr.edu>
2023-06-21 19:27:54 -0400

    ayudaaa
    aaaaaaa

--- Ecommerce/web/frontend/views.py
+++ Ecommerce/web/frontend/views.py
@@ -39,12 +39,24 @@ def profile():
 @login_required
 def editinfo():
     if request.method == 'POST':
+        form_name = request.form.get('form_name')
         id = session.get('customer')
-        fname = request.form['C_fname']
-        lname = request.form['C_lname']
-        email = request.form['C_email']
-        edit_prof(id, fname ,lname ,email)
-        user = user_info(id)
+        if form_name == 'form1':
+            fname = request.form['C_fname']
+            lname = request.form['C_lname']
+            email = request.form['C_email']
+            edit_prof(id, fname ,lname ,email)
+        elif form_name == 'form2':
+            Street_name = request.form['aline2']
+            Street_number = request.form['aline1']
+            city = request.form['city']
+            state = request.form['state']
+            zipcode = request.form['zipcode']
+            edit_address(id, Street_number, Street_name, zipcode, city, state)
+        elif form_name == 'form3':
+            number = request.form['number']
+            edit_phone(id,number)
+    user = user_info(id)
     return render_template('profile.html', user1=user)
 
 
--- Ecommerce/web/models/frontend/loginModel.py
+++ Ecommerce/web/models/frontend/loginModel.py
@@ -1,11 +1,12 @@
 from ...dbConnection import *
 
+#register
+
 @DBConnection
 def insert_user(C_Fname, C_Lname, C_Email, C_password, database): #insertar el user en la base de datos
     cursor = database.cursor()
     cursor.execute('INSERT INTO customers (C_Fname, C_Lname, C_Email, C_Password) VALUES (%s, %s, %s, %s)', (C_Fname, C_Lname, C_Email, C_password, ))
     database.commit()
-
 
 @DBConnection
 def email_exists(C_Email, database): #verificar si el email ya existe en la base de datos
@@ -21,8 +22,19 @@ def ID_Email(C_Email, database): #obtener el id dependiendo del email
     result = cursor
     return result
 
+#login
+
 @DBConnection
-def user_info(id, database): #verificar si el email ya existe en la base de datos
+def customerlog(email,database): #Obtiene el password y el customer id del email (gabriel)
+    cursor = database.cursor()
+    cursor.execute('SELECT C_Password,CustomerID FROM customers WHERE C_Email = %s', (email,))
+    customer = cursor.fetchone()
+    return customer
+
+#profile
+
+@DBConnection
+def user_info(id, database): #selecciona informacion deacuerdo al id del usuario
     cursor = database.cursor()
     cursor.execute('SELECT C_Fname, C_Lname, C_Email, C_Password, C_Phone, Street_number, Street_name, zipCode, C_City, C_State FROM customers WHERE CustomerID = %s', (id,))
     result = cursor.fetchone()
@@ -35,8 +47,13 @@ def edit_prof(id, fname, lname, email, database): #edit profile info
     database.commit()
 
 @DBConnection
-def customerlog(email,database): #Obtiene el password y el customer id del email (gabriel)
+def edit_address(id, street_number, street_name, zipcode, c_city, c_state, database): #edit billing addres info
     cursor = database.cursor()
-    cursor.execute('SELECT C_Password,CustomerID FROM customers WHERE C_Email = %s', (email,))
-    customer = cursor.fetchone()
-    return customer
+    cursor.execute('UPDATE customers SET Street_number = %s, Street_name = %s, zipCode = %s, C_City = %s, C_State = %s WHERE CustomerID = %s ', (street_number, street_name, zipcode, c_city, c_state, id)) 
+    database.commit()   
+
+@DBConnection
+def edit_phone(id, C_Phone, database): #edit number 
+    cursor = database.cursor()
+    cursor.execute('UPDATE customers SET C_Phone = %s WHERE CustomerID = %s ', (C_Phone, id)) 
+    database.commit() 
