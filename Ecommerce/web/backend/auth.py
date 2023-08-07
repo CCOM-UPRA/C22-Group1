a6415bb725ef7ddab533faf50baeaae3bc0b1d88
Yadiel Cruzado <yadiel.cruzado@upr.edu>
2023-08-07 16:54:38 -0400

    encriptar password
    passwords encriptados

--- Ecommerce/web/backend/auth.py
+++ Ecommerce/web/backend/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request,Flask,session
+from ..controllers.backend.accountController import *
 
 auth = Blueprint('back_auth', __name__, template_folder='/templates')
 
@@ -8,3 +9,14 @@ app = Flask(__name__)
 def backlogout():
     session.pop('admin')
     return redirect(url_for('auth.login'))
+
+@auth.route('encript', methods=['GET', 'POST'])
+def encript():
+    if request.method == 'POST':
+        users = UsersList()
+        for i in users:
+            user_id = i['id']
+            user_pass = i['pass']
+            pass_hash = hashlib.sha256(user_pass.encode()).hexdigest()
+            #update_password(user_id, pass_hash) 
+    return redirect(url_for('back_views.accounts'))
